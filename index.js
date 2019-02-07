c29ccf1f26e0d7f9415a81f168f1c24b4e70425d
adrioro <artistsvillage@me.com>
2019-02-07 09:57:46 -0500

    Commit 8 Displays "0 $" unless all fields are filled.

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
-function tipFunction() {
+//Main function: "costFunction()"
+function costFunction() { //Getting the input values.
     var myBill = document.getElementById("bill").value;
     var myTip = document.getElementById("tip").value;
     var myPeople = document.getElementById("people").value;
+    var array = [myBill, myTip, myPeople];
 
-    var tipNumb = ((myBill / 100) * myTip) / myPeople; //Tip per person.
-    document.getElementById("tip-per-person").innerHTML = tipNumb + " $";
+    function resultsFunction() {
+        //Tip per person.
+        var tipNumb = ((myBill / 100) * myTip) / myPeople;
+        document.getElementById("tip-per-person").innerHTML = tipNumb + " $";
+        //Total per person.
+        var totalNumb = (myBill / myPeople) + tipNumb;
+        document.getElementById("total-per-person").innerHTML = (totalNumb + " $")
+    };
 
-    var totalNumb = (myBill / myPeople) + tipNumb; //Total per person.
-    document.getElementById("total-per-person").innerHTML = totalNumb + " $";
+    array.forEach(function (element) {
 
-
+        array[2] > 0 ? resultsFunction() : (document.getElementById("tip-per-person").innerHTML) = "0 $";
+    });
 };
